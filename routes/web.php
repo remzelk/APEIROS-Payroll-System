c0b70e99a381b819132c1f7d801f4f8276866c22
remzelk <aliciamaye.mondero@letran.edu.ph>
2022-09-13 11:54:54 +0800

    changes auth

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,23 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::middleware('guest')->group(function () {
+    Route::get('/', 'HomeController@index');
+    Route::get('/Contact', 'HomeController@contact');
+});
 
-require __DIR__.'/auth.php';
-
-Route::get('/', 'HomeController@index');
-Route::get('/Contact', 'HomeController@contact');
-
+Route::middleware('IsAdmin')->group(function () {
 Route::get('/Admin', 'AdminController@index');
 Route::get('/Admin/Profile', 'AdminController@profile');
 Route::get('/Admin/AccountSettings', 'AdminController@accountsettings');
 Route::get('/Admin/Credentials/Register', 'RegisterController@create');
 Route::post('/Admin/Credentials/Register', 'RegisterController@store');
+Route::resource('/Admin/Credentials/Admin', AdminCredentialsController::class);
+Route::resource('/Admin/Credentials/HumanResources', HumanResourcesCredentialsController::class);
+Route::resource('/Admin/Credentials/Accounting', AccountingCredentialsController::class);
+Route::resource('/Admin/Credentials/Employee', EmployeeCredentialsController::class);
+Route::resource('/Admin/EmployeeList', AdminEmployeeController::class);
+});
 
 Route::get('/HumanResources', 'HumanResourcesController@index');
 Route::get('/HumanResources/Profile', 'HumanResourcesController@profile');
@@ -35,14 +37,13 @@ Route::get('/Employee/LeaveRequest', 'EmployeesController@leaverequest');
 Route::get('/Employee/BIRForm2316', 'EmployeesController@bir');
 Route::get('/Employee/AccountSettings', 'EmployeesController@accountsettings');
 
-Route::resource('/Admin/Credentials/Admin', AdminCredentialsController::class);
-Route::resource('/Admin/Credentials/HumanResources', HumanResourcesCredentialsController::class);
-Route::resource('/Admin/Credentials/Accounting', AccountingCredentialsController::class);
-Route::resource('/Admin/Credentials/Employee', EmployeeCredentialsController::class);
-Route::resource('/Admin/EmployeeList', AdminEmployeeController::class);
+
 Route::resource('/HumanResources/EmployeeList', HumanResourcesEmployeeController::class);
 Route::resource('/Admin/Payroll', AdminEmployeePayrollController::class);
 Route::resource('/Accounting/Payroll', AccountingEmployeePayrollController::class);
 Route::resource('/Admin/DetachmentsWages', AdminDetachmentController::class);
 Route::resource('/HumanResources/Detachments', HumanResourcesDetachmentController::class);
 Route::resource('/Accounting/Wages', AccountingWageController::class);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
